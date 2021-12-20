8acd8b380131a5eb2cf63cb89d5777b31751b070
Chris Nicholson <datapolitical@gmail.com>
2021-12-19 22:55:03 -0800

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 export PATH=$PATH:/opt/buildhome/.local/bin
-git clone https://github.com/getpelican/pelican-themes.git /opt/buildhome
+git clone https://github.com/getpelican/pelican-themes.git /opt/buildhome/pelican-bootstrap3
 pip install pelican
 pelican content
