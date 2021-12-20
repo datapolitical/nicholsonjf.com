680e2d4ffa423ae8f2983028166a0e45f003d556
Chris Nicholson <datapolitical@gmail.com>
2021-12-19 23:36:18 -0800

    hmmmm

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 export PATH=$PATH:/opt/buildhome/.local/bin
 git clone https://github.com/getpelican/pelican-themes.git /opt/buildhome/pelican-themes
-echo ls -A $HOME
-echo ls -A $HOME/pelican-themes
+echo ls -l $HOME
+echo ls -l $HOME/pelican-themes
 pip install markdown
 pip install pelican
 pelican content
